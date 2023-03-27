cf9ed8e429f51689c979b05a44ec79ea7a811299
Fatima Tezo <ftezo@nabenik.dev>
2023-03-26 23:13:22 -0600

    Initial commit

---
+++ src/test/java/com/interpackage/resources/ResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interpackage.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
