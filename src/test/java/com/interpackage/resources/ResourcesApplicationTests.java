72897875abc857858b31bc62c46a51953a34f460
Fatima Tezo <ftezo@nabenik.dev>
2023-05-05 23:31:45 -0600

    ADD: Code coverage (Jacoco), JunitTest, Mockito and Docker testcontainers

--- src/test/java/com/interpackage/resources/ResourcesApplicationTests.java
+++ src/test/java/com/interpackage/resources/ResourcesApplicationTests.java
@@ -2,9 +2,12 @@ package com.interpackage.resources;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+
+@Testcontainers
 @SpringBootTest
-class ResourcesApplicationTests {
+class ResourcesApplicationTests extends AbstractIntegrationTest{
 
 	@Test
 	void contextLoads() {
