6c59eb0757e69bc78c582a4557562e1a80038976
BryanGmz <brgg13@gmail.com>
2023-05-11 18:13:20 -0600

    feat: :sparkles: Update Route
    Added service for route update

--- src/test/java/com/interpackage/resources/service/RouteServiceTests.java
+++ src/test/java/com/interpackage/resources/service/RouteServiceTests.java
@@ -1,7 +1,8 @@
 package com.interpackage.resources.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.interpackage.resources.AbstractIntegrationTest;
 import com.interpackage.resources.model.Response;
 import com.interpackage.resources.model.Route;
@@ -10,21 +11,18 @@ import com.interpackage.resources.util.Constants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import java.math.BigDecimal;
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Objects;
 
 @Testcontainers
 @SpringBootTest
-public class RouteServiceTests extends AbstractIntegrationTest {
+class RouteServiceTests extends AbstractIntegrationTest {
 
     @Autowired
     private RouteService routeService;
@@ -32,29 +30,17 @@ public class RouteServiceTests extends AbstractIntegrationTest {
     @Autowired
     private RouteRepository routeRepository;
 
-    @Value("${server.port}")
-    private String port;
-
     private Route route;
 
     @BeforeEach
     void setUp() {
-        routeRepository.deleteAll();
-        this.route = new Route(1L, "Ruta-Test", new BigDecimal("12.50"), 1L, 2L);
+        this.routeRepository.deleteAll();
+        this.route = new Route(1L, "Ruta-Test",  BigDecimal.TEN, 1L, 2L);
     }
 
     @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void createRouteSuccess() throws URISyntaxException {
-        RequestEntity<Route> request = RequestEntity
-                .post(new URI(Constants.HOST + port + "/route/"))
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(route);
-
-        ResponseEntity<Response> responseEntity = routeService.create(Objects.requireNonNull(request.getBody()));
+    void createRouteSuccess() {
+        ResponseEntity<Response> responseEntity = routeService.create(route);
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
         assertTrue(((Route) Objects.requireNonNull(responseEntity.getBody())
                 .getResponseObject())
@@ -62,32 +48,104 @@ public class RouteServiceTests extends AbstractIntegrationTest {
     }
 
     @Test
-    void createRouteAlreadyExists() throws URISyntaxException {
+    void createRouteAlreadyExists() {
         routeRepository.save(route);
-
-        RequestEntity<Route> request = RequestEntity
-                .post(new URI(Constants.HOST + port + "/route/"))
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(route);
-
-        ResponseEntity<Response> responseEntity = routeService.create(Objects.requireNonNull(request.getBody()));
+        ResponseEntity<Response> responseEntity = routeService.create(route);
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
         assertTrue(Objects.requireNonNull(responseEntity.getBody())
                 .getMessage().contains("Ya existe una ruta con el nombre"));
     }
 
     @Test
-    void createRouteMissingFields() throws URISyntaxException {
-        Route route = new Route();
-
-        RequestEntity<Route> request = RequestEntity
-                .post(new URI(Constants.HOST + port + "/routes"))
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(route);
-
-        ResponseEntity<Response> responseEntity = routeService.create(Objects.requireNonNull(request.getBody()));
+    void createRouteMissingFields() {
+        ResponseEntity<Response> responseEntity = routeService.create(new Route());
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
         assertTrue(Objects.requireNonNull(responseEntity.getBody())
-                .getMessage().contains("Todos los campos son requeridos"));
+                .getMessage().contains(Constants.REQUIRED_FIELDS));
+    }
+
+    @Test
+    void editRouteSuccess() throws URISyntaxException {
+        Route routeUpdate = new Route(1L, "Ruta1", BigDecimal.ONE, 1L, 2L);
+        routeRepository.save(route);
+
+        ResponseEntity<Response> response = routeService.edit(routeUpdate);
+
+        // Comprueba que la respuesta sea un 200 OK
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        // Comprueba que se hayan actualizado los datos correctamente
+        Route updatedRoute = (Route) Objects.requireNonNull(routeService.getById(1L).getBody()).getResponseObject();
+        assertThat(updatedRoute.getName()).isEqualTo("Ruta1");
+        assertThat(updatedRoute.getPriceWeight()).isEqualTo(new BigDecimal("1.000000"));
+        assertThat(updatedRoute.getOrigin()).isEqualTo(1L);
+        assertThat(updatedRoute.getDestination()).isEqualTo(2L);
+    }
+
+    @Test
+    void editRouteAlreadyExists() {
+        Route existingRoute = new Route(1L, "Existing Route", BigDecimal.ONE, 1L, 2L);
+        routeRepository.save(existingRoute);
+        Route routeToUpdate = new Route();
+        routeToUpdate.setRouteId(20L);
+        routeToUpdate.setName("Existing Route");
+        routeToUpdate.setPriceWeight(BigDecimal.TEN);
+        routeToUpdate.setOrigin(1L);
+        routeToUpdate.setDestination(2L);
+
+        // Act
+        ResponseEntity<Response> response = routeService.edit(routeToUpdate);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(Objects.requireNonNull(response.getBody()).getMessage()).contains("Ya existe una ruta con el nombre");
+    }
+
+    @Test
+    void editRouteDoesNotExist() {
+        Route routeDoesNotExist = new Route(100L, "Existing Route", BigDecimal.ONE, 1L, 2L);
+        // Act
+        ResponseEntity<Response> response = routeService.edit(routeDoesNotExist);
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void editRouteMissingFields() {
+        Route existingRoute = new Route(1L, "Existing", BigDecimal.ONE, 1L, 2L);
+        routeRepository.save(existingRoute);
+        Route routeMissingFields = new Route();
+        routeMissingFields.setRouteId(3L);
+        routeMissingFields.setName("Test");
+        ResponseEntity<Response> responseEntity = routeService.edit(routeMissingFields);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertTrue(Objects.requireNonNull(responseEntity.getBody())
+                .getMessage().contains(Constants.REQUIRED_FIELDS));
+    }
+
+    @Test
+    void getAllRoutes() {
+        routeRepository.save(new Route(1L, "Existing1", BigDecimal.ONE, 1L, 2L));
+        routeRepository.save(new Route(2L, "Existing2", BigDecimal.ONE, 1L, 2L));
+        routeRepository.save(new Route(3L, "Existing3", BigDecimal.ONE, 1L, 2L));
+
+        // Act
+        ResponseEntity<Response> response = routeService.getAll();
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+
+        List<Route> responseRoutes = (List<Route>) response.getBody().getResponseObject();
+        assertEquals(3, responseRoutes.size());
+    }
+
+    @Test
+    void getRouteMissingFields(){
+        routeRepository.save(new Route(1L, "Existing", BigDecimal.ONE, 1L, 2L));
+        ResponseEntity<Response> responseEntity = routeService.getById(null);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertTrue(Objects.requireNonNull(responseEntity.getBody())
+                .getMessage().contains(Constants.REQUIRED_FIELDS));
     }
 }
