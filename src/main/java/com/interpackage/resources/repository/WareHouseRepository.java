0f5718b6e62577851e50ab7f4f7ad2a5a621af47
Jhonny Ismael García Hernandez <113376063+ismaG12@users.noreply.github.com>
2023-05-13 18:08:17 -0600

    feat: :sparkles: find all warehouse or find by id
    Get all warehouses or just get one by id
    
    #20-list-warehouse

--- src/main/java/com/interpackage/resources/repository/WareHouseRepository.java
+++ src/main/java/com/interpackage/resources/repository/WareHouseRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.interpackage.resources.model.Warehouse;
 
+import java.util.List;
+
 @Repository
 public interface WareHouseRepository extends JpaRepository<Warehouse, Long> {
     boolean existsWarehouseByName(String name);
+    List<Warehouse> findAllByDeleted(boolean deleted);
 }
